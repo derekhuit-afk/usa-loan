c04fcc52deedda63bc5cdfca037c0f3909e87d13
derekhuit-afk <derekhuit@gmail.com>
2026-07-12 11:08:23 -0800

    chore: register IL, IN, MT, OK in states index

--- content/states/index.ts
+++ content/states/index.ts
@@ -5,8 +5,15 @@ import { state as alaska } from "./alaska";
 import { state as texas } from "./texas";
 import { state as georgia } from "./georgia";
 import { state as michigan } from "./michigan";
+import { state as illinois } from "./illinois";
+import { state as indiana } from "./indiana";
+import { state as montana } from "./montana";
+import { state as oklahoma } from "./oklahoma";
 
-export const STATES: StatePage[] = [florida, washington, alaska, texas, georgia, michigan];
+export const STATES: StatePage[] = [
+  florida, washington, alaska, texas, georgia, michigan,
+  illinois, indiana, montana, oklahoma,
+];
 
 export function getState(slug: string): StatePage | undefined {
   return STATES.find((s) => s.slug === slug);
