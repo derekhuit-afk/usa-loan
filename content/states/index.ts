2e53a2e8892e912849d1e32e6e0f7fe8703aa951
derekhuit-afk <derekhuit@gmail.com>
2026-07-11 22:00:54 -0800

    chore: register TX, GA, MI in states index

--- content/states/index.ts
+++ content/states/index.ts
@@ -2,8 +2,11 @@ import type { StatePage } from "../../lib/content";
 import { state as florida } from "./florida";
 import { state as washington } from "./washington";
 import { state as alaska } from "./alaska";
+import { state as texas } from "./texas";
+import { state as georgia } from "./georgia";
+import { state as michigan } from "./michigan";
 
-export const STATES: StatePage[] = [florida, washington, alaska];
+export const STATES: StatePage[] = [florida, washington, alaska, texas, georgia, michigan];
 
 export function getState(slug: string): StatePage | undefined {
   return STATES.find((s) => s.slug === slug);
