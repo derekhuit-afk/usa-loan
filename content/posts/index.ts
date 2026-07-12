6f13caac0d493502fcc5d893ad8eb3c9cff12324
derekhuit-afk <derekhuit@gmail.com>
2026-07-11 21:34:50 -0800

    chore: register p05 in posts index

--- content/posts/index.ts
+++ content/posts/index.ts
@@ -3,8 +3,9 @@ import { post as p01 } from "./p01-jacksonville-dscr-investor-guide";
 import { post as p02 } from "./p02-wshfc-dpa-stacking-guide";
 import { post as p03 } from "./p03-jblm-va-loan-guide-washington";
 import { post as p04 } from "./p04-jacksonville-va-loan-veterans-florida";
+import { post as p05 } from "./p05-washington-va-cash-out-refinance";
 
-export const POSTS: Post[] = [p01, p02, p03, p04].sort(
+export const POSTS: Post[] = [p01, p02, p03, p04, p05].sort(
   (a, b) => (a.publishDate < b.publishDate ? -1 : 1)
 );
 
